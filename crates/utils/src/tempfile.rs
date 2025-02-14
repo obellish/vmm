35abca822206d3cae9f9d422c48dbcf0cad6997d
Owen Bellish <obellish@mpilabels.com>
2025-02-14 10:29:04 -0500

    unix done

--- crates/utils/src/tempfile.rs
+++ crates/utils/src/tempfile.rs
@@ -1,8 +1,13 @@
 use std::{
 	env::temp_dir,
-	ffi::{CString, OsStr},
+	ffi::OsStr,
 	fs::{self, File},
 	path::{Path, PathBuf},
+};
+#[cfg(unix)]
+use std::{
+	ffi::CString,
+	os::unix::{ffi::OsStrExt as _, io::FromRawFd as _},
 };
 
 use super::errno::{Error, Result, errno_result};
@@ -14,5 +19,30 @@ pub struct TempFile {
 }
 
 impl TempFile {
-	pub fn with_prefix(prefix: impl AsRef<OsStr>) -> Result<Self> {}
+	#[cfg(unix)]
+	pub fn with_prefix(prefix: impl AsRef<OsStr>) -> Result<Self> {
+		let mut os_fname = prefix.as_ref().to_os_string();
+		os_fname.push("XXXXXX");
+
+		let c_tempname = CString::new(os_fname.as_bytes()).map_err(|_| Error::new(libc::EINVAL))?;
+		let raw_tempname = c_tempname.into_raw();
+
+		let ret = unsafe { libc::mkstemp(raw_tempname) };
+
+		let c_tempname = unsafe { CString::from_raw(raw_tempname) };
+
+		let fd = match ret {
+			-1 => return errno_result(),
+			_ => ret,
+		};
+
+		let os_tempname = OsStr::from_bytes(c_tempname.as_bytes());
+
+		let file = unsafe { File::from_raw_fd(fd) };
+
+		Ok(Self {
+			path: PathBuf::from(os_tempname),
+			file: Some(file),
+		})
+	}
 }
