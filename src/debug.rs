412ef8f832e482d4f12e60fcaeaf36c4804e8cba
obellish <obellish@mpilabels.com>
2025-02-24 15:02:32 -0500

    work

---
+++ src/debug.rs
@@ -0,0 +1,29 @@
+#[cfg(not(feature = "debug"))]
+use bevy::prelude::*;
+#[cfg(feature = "debug")]
+use bevy::{
+	app::{PluginGroup, PluginGroupBuilder},
+	dev_tools::fps_overlay::FpsOverlayPlugin,
+	diagnostic::{
+		EntityCountDiagnosticsPlugin, LogDiagnosticsPlugin, SystemInformationDiagnosticsPlugin,
+	},
+};
+
+pub struct DebugPlugin;
+
+impl PluginGroup for DebugPlugin {
+	fn build(self) -> PluginGroupBuilder {
+		PluginGroupBuilder::start::<Self>()
+			.add(FpsOverlayPlugin::default())
+			.add(LogDiagnosticsPlugin::default())
+			.add(EntityCountDiagnosticsPlugin)
+			.add(SystemInformationDiagnosticsPlugin)
+	}
+}
+
+#[cfg(not(feature = "debug"))]
+impl Plugin for DebugPlugin {
+	fn build(&self, app: &mut App) {
+		info!("not built with `debug` feature, skipping diagnostics");
+	}
+}
