4ff7449c9030dcfe783a8a8019b9269a6dffb4be
obellish <obellish@mpilabels.com>
2025-03-12 13:45:12 -0400

    add new bench

--- crates/goap_pathfinding/benches/algos.rs
+++ crates/goap_pathfinding/benches/algos.rs
@@ -244,6 +244,62 @@ fn corner_to_corner_fringe(c: &mut Criterion) {
 	});
 }
 
+fn corner_to_corner_idastar(c: &mut Criterion) {
+	let mut group = c.benchmark_group(stringify!(corner_to_corner_idastar));
+	group.bench_function("small struct", |b| {
+		b.iter(|| {
+			assert_ne!(
+				idastar(
+					&Pt::new(0, 0),
+					|n| Pt::successors(n).into_iter().map(|n| (n, 1)),
+					Pt::heuristic,
+					Pt::correct
+				),
+				None
+			);
+		});
+	});
+	group.bench_function("large struct", |b| {
+		b.iter(|| {
+			assert_ne!(
+				idastar(
+					&BigPt::new(0, 0),
+					|n| BigPt::successors(n).into_iter().map(|n| (n, 1)),
+					BigPt::heuristic,
+					BigPt::correct
+				),
+				None
+			);
+		});
+	});
+}
+
+fn corner_to_corner_iddfs(c: &mut Criterion) {
+	let mut group = c.benchmark_group(stringify!(corner_to_corner_iddfs));
+	group.bench_function("small struct", |b| {
+		b.iter(|| {
+			assert_ne!(
+				iddfs(Pt::new(0, 0), Pt::successors, |n| matches!(
+					n,
+					Pt { x: 5, y: 5 }
+				)),
+				None
+			);
+		});
+	});
+	group.bench_function("large struct", |b| {
+		b.iter(|| {
+			assert_ne!(
+				iddfs(BigPt::new(0, 0), BigPt::successors, |n| matches!(
+					n,
+					BigPt { x: 5, y: 5, .. }
+				)),
+				None
+			);
+		});
+	});
+}
+
 fn no_path_astar(c: &mut Criterion) {
 	let mut group = c.benchmark_group(stringify!(no_path_astar));
 	group.bench_function("small struct", |b| {
@@ -288,6 +344,147 @@ fn no_path_bfs(c: &mut Criterion) {
 	});
 }
 
+fn no_path_bfs_bidirectional(c: &mut Criterion) {
+	c.bench_function(stringify!(no_path_bfs_bidirectional), |b| {
+		b.iter(|| {
+			assert!(
+				bfs_bidirectional(
+					&Pt::new(2, 3),
+					&Pt::new(u16::MAX, u16::MAX),
+					Pt::successors,
+					|_| Vec::new()
+				)
+				.is_none()
+			);
+		});
+	});
+}
+
+fn no_path_dfs(c: &mut Criterion) {
+	let mut group = c.benchmark_group(stringify!(no_path_dfs));
+	group.bench_function("small struct", |b| {
+		b.iter(|| {
+			assert!(bfs(&Pt::new(2, 3), Pt::successors, |_| false).is_none());
+		});
+	});
+	group.bench_function("large struct", |b| {
+		b.iter(|| {
+			assert!(bfs(&BigPt::new(2, 3), BigPt::successors, |_| false).is_none());
+		});
+	});
+}
+
+fn no_path_dijkstra(c: &mut Criterion) {
+	let mut group = c.benchmark_group(stringify!(no_path_dijkstra));
+	group.bench_function("small struct", |b| {
+		b.iter(|| {
+			assert!(
+				dijkstra(
+					&Pt::new(2, 3),
+					|n| Pt::successors(n).into_iter().map(|n| (n, 1)),
+					|_| false
+				)
+				.is_none()
+			);
+		});
+	});
+	group.bench_function("large struct", |b| {
+		b.iter(|| {
+			assert!(
+				dijkstra(
+					&BigPt::new(2, 3),
+					|n| BigPt::successors(n).into_iter().map(|n| (n, 1)),
+					|_| false
+				)
+				.is_none()
+			);
+		});
+	});
+}
+
+fn no_path_fringe(c: &mut Criterion) {
+	let mut group = c.benchmark_group(stringify!(no_path_fringe));
+	group.bench_function("small struct", |b| {
+		b.iter(|| {
+			assert!(
+				fringe(
+					&Pt::new(2, 3),
+					|n| Pt::successors(n).into_iter().map(|n| (n, 1)),
+					|_| 1,
+					|_| false
+				)
+				.is_none()
+			);
+		});
+	});
+	group.bench_function("large struct", |b| {
+		b.iter(|| {
+			assert!(
+				fringe(
+					&BigPt::new(2, 3),
+					|n| BigPt::successors(n).into_iter().map(|n| (n, 1)),
+					|_| 1,
+					|_| false
+				)
+				.is_none()
+			);
+		});
+	});
+}
+
+#[expect(clippy::set_contains_or_insert)]
+fn bench_separate_components(c: &mut Criterion) {
+	c.bench_function(stringify!(separate_components), |b| {
+		let mut rng = XorShiftRng::from_seed([
+			3, 42, 93, 129, 1, 85, 72, 42, 84, 23, 95, 212, 253, 10, 4, 2,
+		]);
+		let mut seen = HashSet::new();
+		let mut components = (0..100)
+			.map(|_| {
+				let mut component = Vec::new();
+				for _ in 0..100 {
+					let node = rng.next_u64();
+					if !seen.contains(&node) {
+						seen.insert(node);
+						component.push(node);
+					}
+				}
+
+				component.sort_unstable();
+
+				assert!(
+					!component.is_empty(),
+					"component is empty, rng seed needs changed"
+				);
+				component
+			})
+			.collect_vec();
+
+		components.sort_by_key(|c| c[0]);
+		let mut groups = components
+			.iter()
+			.flat_map(|component| {
+				let mut component = component.clone();
+				component.shuffle(&mut rng);
+				let mut subcomponents = Vec::new();
+				while !component.is_empty() {
+					let cut = rng.random_range(0..component.len());
+					let mut subcomponent = component.drain(cut..).collect_vec();
+					if !component.is_empty() {
+						subcomponent.push(component[0]);
+					}
+					subcomponents.shuffle(&mut rng);
+					subcomponents.push(subcomponent);
+				}
+
+				subcomponents
+			})
+			.collect_vec();
+		groups.shuffle(&mut rng);
+		b.iter(|| separate_components(&groups));
+	});
+}
+
 criterion_group!(
 	benches,
 	corner_to_corner_astar,
@@ -296,7 +493,14 @@ criterion_group!(
 	corner_to_corner_dfs,
 	corner_to_corner_dijkstra,
 	corner_to_corner_fringe,
+	corner_to_corner_idastar,
+	corner_to_corner_iddfs,
 	no_path_astar,
-	no_path_bfs
+	no_path_bfs,
+	no_path_bfs_bidirectional,
+	no_path_dfs,
+	no_path_dijkstra,
+	no_path_fringe,
+	bench_separate_components
 );
 criterion_main!(benches);
