8249923cee3ef7111a5c636683c5ee1b3aa0fcfa
obellish <obellish@mpilabels.com>
2025-03-26 15:00:20 -0400

    some spatial work

---
+++ crates/spatial/src/bvh.rs
@@ -0,0 +1,298 @@
+use std::{iter::FusedIterator, mem};
+
+use approx::abs_diff_eq;
+use rayon::iter::{
+	IndexedParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator,
+};
+use vek::{Aabb, Vec3};
+
+use super::{Bounded3D, RaycastHit, SpatialIndex, ray_box_intersect};
+
+#[derive(Debug, Clone)]
+pub struct Bvh<T> {
+	internal_nodes: Vec<InternalNode>,
+	leaf_nodes: Vec<T>,
+	root: NodeIdx,
+}
+
+impl<T> Bvh<T>
+where
+	T: Bounded3D + Send + Sync,
+{
+	#[must_use]
+	pub const fn new() -> Self {
+		Self {
+			internal_nodes: Vec::new(),
+			leaf_nodes: Vec::new(),
+			root: NodeIdx::MAX,
+		}
+	}
+
+	pub fn rebuild(&mut self, leaves: impl IntoIterator<Item = T>) {
+		self.internal_nodes.clear();
+		self.leaf_nodes.clear();
+
+		self.leaf_nodes.extend(leaves);
+
+		let leaf_count = self.leaf_nodes.len();
+
+		if matches!(leaf_count, 0) {
+			return;
+		}
+
+		self.internal_nodes.reserve_exact(leaf_count - 1);
+		self.internal_nodes.resize(
+			leaf_count - 1,
+			InternalNode {
+				bb: Aabb::default(),
+				left: NodeIdx::MAX,
+				right: NodeIdx::MAX,
+			},
+		);
+
+		if NodeIdx::try_from(leaf_count)
+			.ok()
+			.and_then(|count| count.checked_add(count - 1))
+			.is_none()
+		{
+			panic!("too many elements in BVH");
+		}
+
+		let id = self.leaf_nodes[0].aabb();
+		let scene_bounds = self
+			.leaf_nodes
+			.par_iter()
+			.map(Bounded3D::aabb)
+			.reduce(|| id, Aabb::union);
+
+		self.root = rebuild_rec(
+			0,
+			scene_bounds,
+			&mut self.internal_nodes,
+			&mut self.leaf_nodes,
+			leaf_count as NodeIdx,
+		)
+		.0;
+
+		debug_assert_eq!(self.internal_nodes.len(), self.leaf_nodes.len() - 1);
+	}
+}
+
+impl<T> Default for Bvh<T>
+where
+	T: Bounded3D + Send + Sync,
+{
+	fn default() -> Self {
+		Self::new()
+	}
+}
+
+#[derive(Debug)]
+pub struct Internal<'a, T> {
+	bvh: &'a Bvh<T>,
+	idx: NodeIdx,
+}
+
+impl<'a, T> Internal<'a, T> {
+	#[must_use]
+	pub fn split(self) -> (Aabb<f64>, Node<'a, T>, Node<'a, T>) {
+		let internal = &self.bvh.internal_nodes[self.idx as usize];
+
+		let bb = internal.bb;
+		let left = Node::from_idx(self.bvh, internal.left);
+		let right = Node::from_idx(self.bvh, internal.right);
+
+		(bb, left, right)
+	}
+}
+
+impl<T> Bounded3D for Internal<'_, T> {
+	fn aabb(&self) -> Aabb<f64> {
+		self.bvh.internal_nodes[self.idx as usize].bb
+	}
+}
+
+impl<T> Clone for Internal<'_, T> {
+	fn clone(&self) -> Self {
+		*self
+	}
+}
+
+impl<T> Copy for Internal<'_, T> {}
+
+#[derive(Debug, Clone)]
+struct InternalNode {
+	bb: Aabb<f64>,
+	left: NodeIdx,
+	right: NodeIdx,
+}
+
+#[derive(Debug)]
+pub enum Node<'a, T> {
+	Internal(Internal<'a, T>),
+	Leaf(&'a T),
+}
+
+impl<'a, T> Node<'a, T> {
+	fn from_idx(bvh: &'a Bvh<T>, idx: NodeIdx) -> Self {
+		if idx < bvh.internal_nodes.len() as NodeIdx {
+			Self::Internal(Internal { bvh, idx })
+		} else {
+			Self::Leaf(&bvh.leaf_nodes[(idx - bvh.internal_nodes.len() as NodeIdx) as usize])
+		}
+	}
+}
+
+impl<T: Bounded3D> Bounded3D for Node<'_, T> {
+	fn aabb(&self) -> Aabb<f64> {
+		match self {
+			Self::Internal(i) => i.aabb(),
+			Self::Leaf(l) => l.aabb(),
+		}
+	}
+}
+
+impl<T> Clone for Node<'_, T> {
+	fn clone(&self) -> Self {
+		*self
+	}
+}
+
+impl<T> Copy for Node<'_, T> {}
+
+type NodeIdx = u32;
+
+fn rebuild_rec<T>(
+	idx: NodeIdx,
+	mut bounds: Aabb<f64>,
+	internal_nodes: &mut [InternalNode],
+	leaf_nodes: &mut [T],
+	total_leaf_count: NodeIdx,
+) -> (NodeIdx, Aabb<f64>)
+where
+	T: Bounded3D + Send,
+{
+	debug_assert_eq!(leaf_nodes.len() - 1, internal_nodes.len());
+
+	if matches!(leaf_nodes.len(), 1) {
+		return (total_leaf_count - 1 + idx, leaf_nodes[0].aabb());
+	}
+
+	loop {
+		debug_assert!(bounds.is_valid());
+
+		let dims = bounds.max - bounds.min;
+
+		let (mut split, bounds_left, bounds_right) = if dims.x >= dims.y && dims.x >= dims.z {
+			let mid = middle(bounds.min.x, bounds.max.x);
+			let [bounds_left, bounds_right] = bounds.split_at_x(mid);
+
+			let p = partition(leaf_nodes, |l| {
+				middle(l.aabb().min.x, l.aabb().max.x) <= mid
+			});
+
+			(p, bounds_left, bounds_right)
+		} else if dims.y >= dims.x && dims.y >= dims.z {
+			let mid = middle(bounds.min.y, bounds.max.y);
+			let [bounds_left, bounds_right] = bounds.split_at_y(mid);
+
+			let p = partition(leaf_nodes, |l| {
+				middle(l.aabb().min.y, l.aabb().max.y) <= mid
+			});
+
+			(p, bounds_left, bounds_right)
+		} else {
+			let mid = middle(bounds.min.z, bounds.max.z);
+			let [bounds_left, bounds_right] = bounds.split_at_z(mid);
+
+			let p = partition(leaf_nodes, |l| {
+				middle(l.aabb().min.z, l.aabb().max.z) <= mid
+			});
+
+			(p, bounds_left, bounds_right)
+		};
+
+		if matches!(split, 0) {
+			if abs_diff_eq!(
+				bounds_right.min,
+				bounds_right.max,
+				epsilon = f64::EPSILON * 100.0
+			) {
+				split += 1;
+			} else {
+				bounds = bounds_right;
+				continue;
+			}
+		} else if split == leaf_nodes.len() {
+			if abs_diff_eq!(
+				bounds_left.min,
+				bounds_left.max,
+				epsilon = f64::EPSILON * 100.0
+			) {
+				split -= 1;
+			} else {
+				bounds = bounds_left;
+				continue;
+			}
+		}
+
+		let (leaves_left, leaves_right) = leaf_nodes.split_at_mut(split);
+
+		let (internal_left, internal_right) = internal_nodes.split_at_mut(split);
+		let (internal, internal_left) = internal_left.split_last_mut().unwrap();
+
+		let ((left, bounds_left), (right, bounds_right)) = rayon::join(
+			|| {
+				rebuild_rec(
+					idx,
+					bounds_left,
+					internal_left,
+					leaves_left,
+					total_leaf_count,
+				)
+			},
+			|| {
+				rebuild_rec(
+					idx + split as NodeIdx,
+					bounds_right,
+					internal_right,
+					leaves_right,
+					total_leaf_count,
+				)
+			},
+		);
+
+		internal.bb = bounds_left.union(bounds_right);
+		internal.left = left;
+		internal.right = right;
+
+		break (idx + split as NodeIdx - 1, internal.bb);
+	}
+}
+
+fn partition<T>(s: &mut [T], mut pred: impl FnMut(&T) -> bool) -> usize {
+	let mut it = s.iter_mut();
+	let mut true_count = 0;
+
+	while let Some(head) = it.find(|x| {
+		if pred(x) {
+			true_count += 1;
+			false
+		} else {
+			true
+		}
+	}) {
+		if let Some(tail) = it.rfind(|x| pred(x)) {
+			mem::swap(head, tail);
+			true_count += 1;
+		} else {
+			break;
+		}
+	}
+
+	true_count
+}
+
+fn middle(a: f64, b: f64) -> f64 {
+	(a + b) / 2.0
+}
