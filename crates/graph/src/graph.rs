fe49bd4ba7c26ce4bc0ea37b3efdbb49d17fe74c
obellish <obellish@mpilabels.com>
2025-04-15 11:46:16 -0400

    blah

--- crates/graph/src/graph.rs
+++
@@ -1,4 +0,0 @@
-pub use super::graph_impl::{
-	DefaultIx, DiGraph, Edge, EdgeIndex, Graph, GraphError, IndexType, Node, NodeIndex, UnGraph,
-	edge_index, node_index,
-};
--- crates/graph/src/util.rs
+++
@@ -1,9 +0,0 @@
-use core::iter;
-
-pub fn enumerate<I: IntoIterator>(iterable: I) -> iter::Enumerate<I::IntoIter> {
-	iterable.into_iter().enumerate()
-}
-
-pub fn zip<I: IntoIterator, J: IntoIterator>(i: I, j: J) -> iter::Zip<I::IntoIter, J::IntoIter> {
-	i.into_iter().zip(j)
-}
