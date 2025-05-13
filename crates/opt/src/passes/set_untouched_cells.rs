693aa653316823e62f1c41bc1e6a4b1bb5ec643f
obellish <obellish@mpilabels.com>
2025-05-13 11:54:02 -0400

    rework a pass

--- crates/opt/src/passes/set_untouched_cells.rs
+++ crates/opt/src/passes/set_untouched_cells.rs
@@ -1,17 +1,55 @@
 use crate::{Change, Instruction, Pass};
 
 #[derive(Debug, Default)]
-pub struct SetUntouchedCellsPass;
+pub struct SetUntouchedCellsPass {
+	hit_pass: bool,
+}
 
 impl Pass for SetUntouchedCellsPass {
 	fn run_pass(&mut self, program: &mut Vec<Instruction>) -> bool {
-		if let Some(Instruction::IncVal(i)) = program.first() {
-			Change::ReplaceOne(Instruction::SetVal(*i as u8)).apply(program, 0, 1);
+		// if let Some(Instruction::IncVal(i)) = program.first() {
+		// 	Change::ReplaceOne(Instruction::SetVal(*i as u8)).apply(program, 0, 1);
 
-			true
-		} else {
-			false
+		// 	true
+		// } else {
+		// 	false
+		// }
+
+		let mut progress = false;
+
+		if self.hit_pass {
+			return progress;
 		}
+
+		let mut changes = Vec::new();
+
+		for (idx, instr) in program.iter_mut().enumerate() {
+			match instr {
+				Instruction::FindZero(_)
+				| Instruction::MoveVal { .. }
+				| Instruction::RawLoop(_) => {
+					self.hit_pass = true;
+					break;
+				}
+				Instruction::MovePtr(i) if *i < 0 => {
+					self.hit_pass = true;
+					break;
+				}
+				Instruction::IncVal(i) => {
+					// *instr = Instruction::SetVal(*i as u8);
+					// Change::ReplaceOne(Instruction::SetVal(*i as u8)).apply(program, idx, 1);
+					changes.push((Change::ReplaceOne(Instruction::SetVal(*i as u8)), idx, 1));
+				}
+				_ => {}
+			}
+		}
+
+		for (change, idx, size) in changes {
+			change.apply(program, idx, size);
+			progress = true;
+		}
+
+		progress
 	}
 
 	fn should_run_on_loop(&self) -> bool {
