1b0e94dbec245478a300776e6d548e572f5a750b
obellish <obellish@mpilabels.com>
2025-05-13 15:54:05 -0400

    remove breaking code

--- crates/opt/src/passes/unroll_constant_loops.rs
+++ crates/opt/src/passes/unroll_constant_loops.rs
@@ -25,6 +25,28 @@ impl PeepholePass for UnrollConstantLoopsPass {
 					_ => None,
 				}
 			}
+			// [Instruction::IncVal(i), Instruction::RawLoop(inner)] if *i > 0 => {
+			// 	println!("{i} {inner:?}");
+			// 	if inner.iter().any(Instruction::has_side_effect) {
+			// 		return None;
+			// 	}
+
+			// 	match inner.as_slice() {
+			// 		[Instruction::IncVal(-1), rest @ ..] | [rest @ .., Instruction::IncVal(-1)] => {
+			// 			let mut output =
+			// 				Vec::with_capacity((*i as usize) * rest.len() + inner.len());
+
+			// 			for _ in 0..*i {
+			// 				output.extend_from_slice(rest);
+			// 			}
+
+			// 			output.push(Instruction::RawLoop(inner.clone()));
+
+			// 			Some(Change::Replace(output))
+			// 		}
+			// 		_ => None,
+			// 	}
+			// }
 			_ => None,
 		}
 	}
