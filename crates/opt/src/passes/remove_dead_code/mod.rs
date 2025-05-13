fd2db0f648b1838d28ef316f5814992a8de10be9
obellish <obellish@mpilabels.com>
2025-05-13 15:12:22 -0400

    fmt

--- crates/opt/src/passes/remove_dead_code/mod.rs
+++ crates/opt/src/passes/remove_dead_code/mod.rs
@@ -3,6 +3,4 @@ mod empty_loops;
 mod redundancies;
 mod unreachable_loops;
 
-pub use self::{
-	clean_up_start::*, empty_loops::*, redundancies::*, unreachable_loops::*,
-};
+pub use self::{clean_up_start::*, empty_loops::*, redundancies::*, unreachable_loops::*};
