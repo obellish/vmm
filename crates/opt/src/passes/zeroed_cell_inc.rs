1ea97d1d6a52c8c8f0886d4e4d49a55f19413a45
obellish <obellish@mpilabels.com>
2025-05-27 15:58:08 -0400

    more things

--- crates/opt/src/passes/zeroed_cell_inc.rs
+++ crates/opt/src/passes/zeroed_cell_inc.rs
@@ -11,33 +11,13 @@ impl PeepholePass for OptimizeZeroedCellIncValPass {
 	fn run_pass(&mut self, window: &[Instruction]) -> Option<Change> {
 		match window {
 			[
-				Instruction::ScaleAndMoveVal { offset, factor },
+				i,
 				Instruction::IncVal {
 					value,
 					offset: None,
 				},
-			] => Some(Change::Replace(vec![
-				Instruction::scale_and_move_val(*offset, *factor),
-				Instruction::set_val(*value as u8),
-			])),
-			[
-				Instruction::FindZero(i),
-				Instruction::IncVal {
-					offset: None,
-					value,
-				},
-			] => Some(Change::Replace(vec![
-				Instruction::find_zero(*i),
-				Instruction::set_val(*value as u8),
-			])),
-			[
-				dyn_loop @ Instruction::DynamicLoop(..),
-				Instruction::IncVal {
-					value,
-					offset: None,
-				},
-			] => Some(Change::Replace(vec![
-				dyn_loop.clone(),
+			] if i.is_zeroing_cell() => Some(Change::Replace(vec![
+				i.clone(),
 				Instruction::set_val(*value as u8),
 			])),
 			_ => None,
@@ -45,6 +25,6 @@ impl PeepholePass for OptimizeZeroedCellIncValPass {
 	}
 
 	fn should_run(&self, window: &[Instruction]) -> bool {
-		matches!(window, [i, Instruction::IncVal { offset: None, .. }] if i.is_zeroing_cell() && !i.is_clear_val())
+		matches!(window, [i, Instruction::IncVal { offset: None, .. }] if i.is_zeroing_cell())
 	}
 }
