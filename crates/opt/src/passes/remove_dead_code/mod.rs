77b93600d79fd323489a3a855e28fe786575877f
obellish <obellish@mpilabels.com>
2025-05-27 11:52:26 -0400

    fix

--- crates/opt/src/passes/remove_dead_code/mod.rs
+++ crates/opt/src/passes/remove_dead_code/mod.rs
@@ -3,7 +3,9 @@ mod no_move_or_change;
 mod non_movements;
 mod redundancies;
 mod unreachable_loops;
+mod unused_starting_instr;
 
 pub use self::{
 	empty_loops::*, no_move_or_change::*, non_movements::*, redundancies::*, unreachable_loops::*,
+	unused_starting_instr::*,
 };
--- crates/opt/src/passes/remove_dead_code/redundancies/mod.rs
+++ crates/opt/src/passes/remove_dead_code/redundancies/mod.rs
@@ -1,4 +1,3 @@
-mod useless_starting_instr;
 mod writes;
 
-pub use self::{useless_starting_instr::*, writes::*};
+pub use self::writes::*;
