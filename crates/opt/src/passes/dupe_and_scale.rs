0d18f59249042ce7e1d68413a6ad09b6aaea3b00
Owen Bellish <obellish@mpilabels.com>
2025-06-01 10:32:36 -0400

    another dupe pass

---
+++ crates/opt/src/passes/dupe_and_scale.rs
@@ -0,0 +1,60 @@
+use vmm_ir::{Instruction, Offset, SimdInstruction};
+
+use crate::{Change, LoopPass};
+
+#[derive(Debug, Default)]
+pub struct OptimizeDupeAndScaleValPass;
+
+impl LoopPass for OptimizeDupeAndScaleValPass {
+	fn run_pass(&mut self, loop_values: &[Instruction]) -> Option<Change> {
+		let [
+			Instruction::IncVal {
+				value: -1,
+				offset: None,
+			},
+			Instruction::Simd(SimdInstruction::IncVals { value, offsets }),
+		] = loop_values
+		else {
+			return None;
+		};
+
+		let value = *value as u8;
+
+		let mut output = vec![Instruction::dupe_val(
+			offsets.iter().copied().flatten().collect(),
+		)];
+
+		for offset in offsets.iter().copied().flatten().filter_map(|offset| {
+			if let Offset::Relative(offset) = offset {
+				Some(offset)
+			} else {
+				None
+			}
+		}) {
+			output.extend([
+				Instruction::move_ptr(offset),
+				Instruction::scale_val(value),
+				Instruction::move_ptr(-offset),
+			]);
+		}
+
+		Some(Change::Replace(output))
+	}
+
+	fn should_run(&self, loop_values: &[Instruction]) -> bool {
+		matches!(
+			loop_values,
+			[
+				Instruction::IncVal {
+					offset: None,
+					value: -1
+				},
+				Instruction::Simd(SimdInstruction::IncVals {
+					offsets,
+					value
+				})
+			]
+			if !offsets.contains(&None) && !matches!(value, 1)
+		)
+	}
+}
--- crates/opt/src/passes/mod.rs
+++ crates/opt/src/passes/mod.rs
@@ -3,6 +3,7 @@ mod clear_loop;
 mod collapse_relative_instr;
 mod collapse_stacked_instr;
 mod combine_move_change;
+mod dupe_and_scale;
 mod dupe_val;
 mod fetch_value;
 mod find_zero;
@@ -21,7 +22,7 @@ mod zeroed_cell_inc;
 
 pub use self::{
 	clear_cell::*, clear_loop::*, collapse_relative_instr::*, collapse_stacked_instr::*,
-	combine_move_change::*, dupe_val::*, fetch_value::*, find_zero::*, if_nz::*, move_value::*,
-	remove_dead_code::*, reorder_instr::*, scale_val::*, set_scale::*, set_zero::*, simd::*,
-	take_val::*, unroll_constant_loops::*, unroll_increment_loops::*, zeroed_cell_inc::*,
+	combine_move_change::*, dupe_and_scale::*, dupe_val::*, fetch_value::*, find_zero::*, if_nz::*,
+	move_value::*, remove_dead_code::*, reorder_instr::*, scale_val::*, set_scale::*, set_zero::*,
+	simd::*, take_val::*, unroll_constant_loops::*, unroll_increment_loops::*, zeroed_cell_inc::*,
 };
