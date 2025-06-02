853dd7b6464abcb273acef5596a63d635ec644e0
obellish <obellish@mpilabels.com>
2025-06-02 11:35:06 -0400

    more advanced sorting

--- crates/opt/src/passes/reorder_instr/sort/inc.rs
+++ crates/opt/src/passes/reorder_instr/sort/inc.rs
@@ -1,4 +1,6 @@
-use vmm_ir::Instruction;
+use std::cmp::Ordering;
+
+use vmm_ir::{Instruction, Offset, SimdInstruction};
 use vmm_utils::Sorted;
 
 use crate::{Change, PeepholePass};
@@ -10,20 +12,39 @@ impl PeepholePass for SortIncInstrPass {
 	const SIZE: usize = 2;
 
 	fn run_pass(&mut self, window: &[Instruction]) -> Option<Change> {
-		match window {
-			window
-				if window.iter().all(Instruction::is_inc_val)
-					&& !window.is_sorted_by_key(Instruction::offset) =>
-			{
-				Some(Change::Replace(
-					window.to_owned().sorted_by_key(Instruction::offset),
-				))
-			}
-			_ => None,
-		}
+		// match window {
+		// 	window
+		// 		if window.iter().all(Instruction::is_inc_val)
+		// 			&& !window.is_sorted_by_key(offsets_and_values) =>
+		// 	{
+		// 		Some(Change::Replace(
+		// 			window.to_owned().sorted_by_key(offsets_and_values),
+		// 		))
+		// 	}
+		// 	_ => None,
+		// }
+		Some(Change::Replace(window.to_owned().sorted_by(sorter)))
 	}
 
 	fn should_run(&self, window: &[Instruction]) -> bool {
-		window.iter().all(Instruction::is_inc_val) && !window.is_sorted_by_key(Instruction::offset)
+		window.iter().all(Instruction::is_inc_val) && {
+			let cloned = window.to_owned();
+
+			cloned.sorted_by(sorter) != window
+		}
+	}
+}
+
+fn sorter(a: &Instruction, b: &Instruction) -> Ordering {
+	a.offset()
+		.cmp(&b.offset())
+		.then(get_inc_value(a).cmp(&get_inc_value(b)))
+}
+
+const fn get_inc_value(i: &Instruction) -> Option<i8> {
+	match i {
+		Instruction::Simd(SimdInstruction::IncVals { value, .. })
+		| Instruction::IncVal { value, .. } => Some(*value),
+		_ => None,
 	}
 }
