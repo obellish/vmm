42bc3eb18137b824c456ea5478531a1d952a6afe
obellish <obellish@mpilabels.com>
2025-06-10 13:23:42 -0400

    signed casts

--- crates/num/src/ops/cast/signed.rs
+++ crates/num/src/ops/cast/signed.rs
@@ -1,12 +1,25 @@
 use super::CastTo;
 
-pub trait CastToI8: CastTo<i8> {
-	fn cast_to_i8(self) -> i8
-	where
-		Self: Sized,
-	{
-		self.cast_to()
-	}
-}
+pub trait CastToI8: CastTo<i8> {}
 
 impl<T> CastToI8 for T where T: CastTo<i8> {}
+
+pub trait CastToI16: CastTo<i16> {}
+
+impl<T> CastToI16 for T where T: CastTo<i16> {}
+
+pub trait CastToI32: CastTo<i32> {}
+
+impl<T> CastToI32 for T where T: CastTo<i32> {}
+
+pub trait CastToI64: CastTo<i64> {}
+
+impl<T> CastToI64 for T where T: CastTo<i64> {}
+
+pub trait CastToI128: CastTo<i128> {}
+
+impl<T> CastToI128 for T where T: CastTo<i128> {}
+
+pub trait CastToIsize: CastTo<isize> {}
+
+impl<T> CastToIsize for T where T: CastTo<isize> {}
