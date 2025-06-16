8f536f8f7b6eedc0481a21958e89162e36ba8578
obellish <obellish@mpilabels.com>
2025-06-16 15:28:24 -0400

    tests for get_or_zero and heap_size utils

--- crates/utils/src/get_or_zero.rs
+++ crates/utils/src/get_or_zero.rs
@@ -21,8 +21,46 @@ macro_rules! impl_get_or_zero {
                     ::core::num::NonZero::get(self)
                 }
             }
+
+            impl $crate::GetOrZero<$ty> for ::core::option::Option<$ty> {
+                fn get_or_zero(self) -> $ty {
+                    match self {
+                        ::core::option::Option::None => 0,
+                        ::core::option::Option::Some(v) => v,
+                    }
+                }
+            }
+
+            impl $crate::GetOrZero<$ty> for $ty {
+                fn get_or_zero(self) -> $ty {
+                    self
+                }
+            }
         )*
     };
 }
 
 impl_get_or_zero!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+
+#[cfg(test)]
+mod tests {
+	use core::num::NonZero;
+
+	use quickcheck::{TestResult, quickcheck};
+
+	use super::GetOrZero;
+
+	fn u8_check(value: u8) -> TestResult {
+		TestResult::from_bool(NonZero::new(value).get_or_zero() == value)
+	}
+
+	fn u16_check(value: u16) -> TestResult {
+		TestResult::from_bool(NonZero::new(value).get_or_zero() == value)
+	}
+
+	#[test]
+	fn unsigned_works() {
+		quickcheck(u8_check as fn(u8) -> TestResult);
+		quickcheck(u16_check as fn(u16) -> TestResult);
+	}
+}
--- crates/utils/src/heap_size.rs
+++ crates/utils/src/heap_size.rs
@@ -58,3 +58,51 @@ where
 		}
 	}
 }
+
+#[cfg(test)]
+#[allow(unused_allocation)]
+mod tests {
+	use alloc::{borrow::Cow, boxed::Box, string::String, vec::Vec};
+
+	use quickcheck::{TestResult, quickcheck};
+
+	use super::HeapSize;
+
+	fn string_impl(a: String) -> TestResult {
+		TestResult::from_bool(a.heap_size() == a.len())
+	}
+
+	fn vec_impl<T>(vec: Vec<T>) -> TestResult {
+		TestResult::from_bool(vec.heap_size() == vec.capacity() * core::mem::size_of::<T>())
+	}
+
+	#[test]
+	fn basic() {
+		quickcheck(string_impl as fn(String) -> TestResult);
+		quickcheck(vec_impl as fn(Vec<u8>) -> TestResult);
+		quickcheck(vec_impl as fn(Vec<u16>) -> TestResult);
+		quickcheck(vec_impl as fn(Vec<()>) -> TestResult);
+	}
+
+	#[test]
+	fn empty_sizes() {
+		assert_eq!(Vec::<u32>::new().heap_size(), 0);
+		assert_eq!(Box::new(()).heap_size(), 0);
+		assert_eq!(Vec::<u32>::new().into_boxed_slice().heap_size(), 0);
+
+		assert_eq!(String::new().heap_size(), 0);
+		assert_eq!(Option::<String>::None.heap_size(), 0);
+
+		assert_eq!(Cow::Borrowed("hello, world!").heap_size(), 0);
+	}
+
+	#[test]
+	fn cow() {
+		let mut c = Cow::Borrowed(&[0u8, 1, 2, 3] as &[u8]);
+		assert_eq!(c.heap_size(), 0);
+
+		c.to_mut();
+
+		assert_eq!(c.heap_size(), 4);
+	}
+}
