6b869484ed4dbedf1201481f340abca369bc7c5e
obellish <obellish@mpilabels.com>
2025-06-17 12:10:40 -0400

    remove commented out tests

--- crates/utils/src/get_or_zero.rs
+++ crates/utils/src/get_or_zero.rs
@@ -46,24 +46,9 @@ impl_get_or_zero!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
 mod tests {
 	use core::num::NonZero;
 
-	// use quickcheck::{TestResult, quickcheck};
 	use vmm_testing::run_test;
 
 	use super::GetOrZero;
-
-	// fn u8_check(value: u8) -> TestResult {
-	// 	TestResult::from_bool(NonZero::new(value).get_or_zero() == value)
-	// }
-
-	// fn u16_check(value: u16) -> TestResult {
-	// 	TestResult::from_bool(NonZero::new(value).get_or_zero() == value)
-	// }
-
-	// #[test]
-	// fn unsigned_works() {
-	// 	quickcheck(u8_check as fn(u8) -> TestResult);
-	// 	quickcheck(u16_check as fn(u16) -> TestResult);
-	// }
 
 	#[test]
 	fn unsigned_works() {
