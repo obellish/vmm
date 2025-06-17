384bbfb01515f37e9fafc770852d770a16a53dda
obellish <obellish@mpilabels.com>
2025-06-17 11:43:58 -0400

    new tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ mod tests {
 
 	use vmm_interpret::Interpreter;
 	use vmm_opt::{HashMetadataStore, Optimizer};
-use vmm_parse::{ParseError, Parser};
+	use vmm_parse::{ParseError, Parser};
 	use vmm_program::Program;
 
-    const HELLO_WORLD_RAW: &str = include_str!("../programs/hello_world.bf");
+	const HELLO_WORLD_RAW: &str = include_str!("../programs/hello_world.bf");
 
 	fn get_program(raw: &'static str) -> Result<Program, ParseError> {
 		Parser::new(raw)
@@ -17,39 +17,37 @@ use vmm_parse::{ParseError, Parser};
 			.map(|v| v.into_iter().collect::<Program>())
 	}
 
-	#[test]
-	fn hello_world_unoptimized() -> anyhow::Result<()> {
-		let program = get_program(HELLO_WORLD_RAW)?;
+	fn run_program(program: &'static str, optimized: bool) -> anyhow::Result<Vec<u8>> {
+		let program = get_program(program)?;
 
 		let output = {
+			let program = if optimized {
+				Optimizer::new(program, HashMetadataStore::new()).optimize()?
+			} else {
+				program
+			};
+
 			let mut interpreter = Interpreter::new(program, io::empty(), Vec::<u8>::new());
 
 			interpreter.run()?;
 
-            interpreter.output().clone()
+			interpreter.output().clone()
 		};
 
-        assert_eq!(output, b"Hello World!\n");
+		Ok(output)
+	}
+
+	#[test]
+	fn hello_world_unoptimized() -> anyhow::Result<()> {
+		assert_eq!(run_program(HELLO_WORLD_RAW, false)?, b"Hello World!\n");
 
 		Ok(())
 	}
 
-    #[test]
-    fn hello_world_optimized() ->anyhow::Result<()> {
-        let program = get_program(HELLO_WORLD_RAW)?;
+	#[test]
+	fn hello_world_optimized() -> anyhow::Result<()> {
+		assert_eq!(run_program(HELLO_WORLD_RAW, true)?, b"Hello World!\n");
 
-        let program = Optimizer::new(program, HashMetadataStore::new()).optimize()?;
-
-        let output = {
-            let mut interpreter = Interpreter::new(program, io::empty(), Vec::<u8>::new());
-
-            interpreter.run()?;
-
-            interpreter.output().clone()
-        };
-
-        assert_eq!(output, b"Hello World!\n");
-
-        Ok(())
-    }
+		Ok(())
+	}
 }
