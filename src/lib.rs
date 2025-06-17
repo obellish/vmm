9f48497430964fd710aacf5475aae058165236e8
obellish <obellish@mpilabels.com>
2025-06-17 11:57:38 -0400

    too long

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,11 @@ mod tests {
 	use vmm_parse::{ParseError, Parser};
 	use vmm_program::Program;
 
-	const HELLO_WORLD_RAW: &str = include_str!("../programs/hello_world.bf");
+	const HELLO_WORLD: &str = include_str!("../programs/hello_world.bf");
+
+	const HELLO_WORLD_TEST: &str = include_str!("../programs/hello_world_test.bf");
+
+	const BENCH: &str = include_str!("../programs/bench.bf");
 
 	fn get_program(raw: &'static str) -> Result<Program, ParseError> {
 		Parser::new(raw)
@@ -39,14 +43,38 @@ mod tests {
 
 	#[test]
 	fn hello_world_unoptimized() -> anyhow::Result<()> {
-		assert_eq!(run_program(HELLO_WORLD_RAW, false)?, b"Hello World!\n");
+		assert_eq!(run_program(HELLO_WORLD, false)?, b"Hello World!\n");
 
 		Ok(())
 	}
 
 	#[test]
 	fn hello_world_optimized() -> anyhow::Result<()> {
-		assert_eq!(run_program(HELLO_WORLD_RAW, true)?, b"Hello World!\n");
+		assert_eq!(run_program(HELLO_WORLD, true)?, b"Hello World!\n");
+
+		Ok(())
+	}
+
+	#[test]
+	fn hello_world_test_unoptimized() -> anyhow::Result<()> {
+		assert_eq!(run_program(HELLO_WORLD_TEST, false)?, b"Hello World! 255\n");
+
+		Ok(())
+	}
+
+	#[test]
+	#[cfg_attr(miri, ignore)]
+	fn hello_world_test_optimized() -> anyhow::Result<()> {
+		assert_eq!(run_program(HELLO_WORLD_TEST, true)?, b"Hello World! 255\n");
+
+		Ok(())
+	}
+
+	// We only run optimized bc unoptimized takes too long
+	#[test]
+	#[cfg_attr(miri, ignore)]
+	fn bench_optimized() -> anyhow::Result<()> {
+		assert_eq!(run_program(BENCH, true)?, b"ZYXWVUTSRQPONMLKJIHGFEDCBA\n");
 
 		Ok(())
 	}
