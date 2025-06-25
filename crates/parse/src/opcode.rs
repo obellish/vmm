6fd14273c8bf56d426f511b3d66c50a6a4f439a7
obellish <obellish@mpilabels.com>
2025-06-25 10:03:31 -0400

    log the raw op code

--- crates/parse/src/opcode.rs
+++ crates/parse/src/opcode.rs
@@ -1,3 +1,5 @@
+use core::fmt::{Display, Formatter, Result as FmtResult, Write as _};
+
 use logos::Logos;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Logos)]
@@ -18,4 +20,19 @@ pub enum OpCode {
 	JumpLeft,
 	#[token("[")]
 	JumpRight,
+}
+
+impl Display for OpCode {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		f.write_char(match self {
+			Self::MoveLeft => '<',
+			Self::MoveRight => '>',
+			Self::Increment => '+',
+			Self::Decrement => '-',
+			Self::Input => ',',
+			Self::Output => '.',
+			Self::JumpLeft => ']',
+			Self::JumpRight => '[',
+		})
+	}
 }
