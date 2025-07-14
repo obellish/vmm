30549ff1ce456a2e5459ea9da106814b60e62924
obellish <obellish@mpilabels.com>
2025-07-14 09:44:47 -0400

    finish semantics and add toy lang

--- crates/semantic/tests/binding.rs
+++ crates/semantic/tests/binding.rs
@@ -22,17 +22,62 @@ fn binding_transform() {
 		>::Primitive(PrimitiveValue::U64(3)),
 		operation: None,
 	};
-    let binding_ast = ast::Binding {
-        name: ast::ValueName::new(Ident::new("x")),
-        value: Box::new(expr_ast.clone())
-    };
+	let binding_ast = ast::Binding {
+		name: ast::ValueName::new(Ident::new("x")),
+		value: Box::new(expr_ast.clone()),
+	};
 
-    assert_eq!(binding_ast.location(), CodeLocation::new(1, 0));
-    assert_eq!(binding_ast.name(), "x");
+	assert_eq!(binding_ast.location(), CodeLocation::new(1, 0));
+	assert_eq!(binding_ast.name(), "x");
 
-    let binding: Binding = binding_ast.clone().into();
-    assert_eq!(binding.to_string(), "x");
-    assert_eq!(binding.value, Box::new(expr_ast.into()));
+	let binding: Binding = binding_ast.clone().into();
+	assert_eq!(binding.to_string(), "x");
+	assert_eq!(binding.value, Box::new(expr_ast.into()));
 
-    dbg!(format!("{binding_ast:?}"));
+	dbg!(format!("{binding_ast:?}"));
+}
+
+#[test]
+fn binding_wrong_expression() {
+	let block_state = Rc::new(RefCell::new(BlockState::new(None)));
+	let mut t = SemanticTest::new();
+
+	let expr = ast::Expression {
+		value: ast::ExpressionValue::<
+			CustomExpressionInstruction,
+			CustomExpression<CustomExpressionInstruction>,
+		>::ValueName(ast::ValueName::new(Ident::new("x"))),
+		operation: None,
+	};
+
+	let binding = ast::Binding {
+		name: ast::ValueName::new(Ident::new("x")),
+		value: Box::new(expr),
+	};
+
+	t.state.binding(&binding, &block_state);
+	assert_eq!(t.errors_len(), 1);
+	assert!(t.check_error(StateErrorType::ValueNotFound));
+}
+
+#[test]
+fn binding_value_not_exist() {
+	let block_state = Rc::new(RefCell::new(BlockState::new(None)));
+	let mut t = SemanticTest::new();
+
+	let expr = ast::Expression {
+		value: ast::ExpressionValue::<
+			CustomExpressionInstruction,
+			CustomExpression<CustomExpressionInstruction>,
+		>::Primitive(PrimitiveValue::I16(23)),
+		operation: None,
+	};
+	let binding = ast::Binding {
+		name: ast::ValueName::new(Ident::new("x")),
+		value: Box::new(expr),
+	};
+
+	t.state.binding(&binding, &block_state);
+	assert_eq!(t.errors_len(), 1);
+	assert!(t.check_error(StateErrorType::ValueNotFound));
 }
